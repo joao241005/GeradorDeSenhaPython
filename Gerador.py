81d70965d7151a5985304e81794ac80878b762f2
joao241005 <106456293+joao241005@users.noreply.github.com>
2023-10-04 21:56:08 -0300

    Gerador.py

--- Gerador.py
+++ Gerador.py
@@ -9,8 +9,7 @@ acentos = ['!','@','#','%','&','*','?','$','°','º','§','_','|','-','~']
 
 # formato da senha
 def config():
-    tamanho = int(input("Digite a quantidade de caracteres que você deseja que sua senha tenha:\n"));
-    caracteres = str(input("Sua senha terá caracteres especiais? Digite 'sim' ou 'não': \n"))
+    caracteres = str(input("Voce prefere uma senha forte, normal ou fraca?: \n"))
     tamanho = int(input("Digite o maximo de caracteres que sua senha deve conter:\n"))
     return caracteres, tamanho
 
@@ -19,17 +18,52 @@ def Senha():
 
     # Definindo se a senha vai ter caracteres especiais
     
-    if caracteres == 'sim':
-        for Gerador in range(1,tamanho+1):
-            senha = [letras,numeros,acentos]
-            lista = random.choice(senha)
-            valor = random.choice(lista)
+    if caracteres == 'Forte' or caracteres == 'forte':
 
-            print(f"{valor}", end='')
-    else:
-        senha = [letras,numeros]
-        lista = random.choice(senha)
+        if tamanho <= 15:
+            print("Sua senha é:")
+
+            for Gerador in range(1,tamanho+1):
+                senha = [letras,numeros,acentos]
+                lista = random.choice(senha)
+                valor = random.choice(lista)
+
+                print(valor, end='')
+        else:
+            print("Invalido.O numero maximo de caracteres é 15")
+        
+    elif caracteres == 'Normal' or caracteres == 'normal':
+        
+        if tamanho < 15:
+            print("Sua senha é:")
+
+            for Gerador in range(1,tamanho+1):
+                senha = [letras,numeros]
+                lista = random.choice(senha)
+                valor = random.choice(lista)
+
+                print(valor, end='')
+        
+        else:
+            print("Invalido.O numero maximo de caracteres é 15")
+
+    elif caracteres == 'Fraca' or caracteres == 'fraca':
+        
+        if tamanho <= 15:
+            print("Sua senha é:")
+
+            for Gerador in range(1,tamanho+1):
+                senha = [letras]
+                lista = random.choice(senha)
+                valor = random.choice(lista)
+
+                print(valor, end='')
+        else:
+            print("Invalido.O numero maximo de caracteres é 15")
+
 
 Senha()
+
+    
 
     
